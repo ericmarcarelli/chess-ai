b59dc2e9f5c44896102adb5407dbf476dd6215c3
Eric Marcarelli <ericmarcarelli@users.noreply.github.com>
2016-12-16 20:54:54 -0500

    Highlight potential moves on selection

--- js/board.js
+++ js/board.js
@@ -2,12 +2,13 @@
   ChessAI.Modules.Board = function() {
     var self = this;
     var $board = $('.board');
+    var P = ChessAI.Piece;
 
     this.init = function() {
       self.setupBoard();
       self.getState();
-      var moves = ChessAI.Modules.States.getMovesForSquare(self.getState(), 1, 1);
-      console.log(moves);
+      self.setupPieceEvents();
+
       return this;
     };
 
@@ -17,14 +18,14 @@
      */
     this.setupBoard = function() {
       var state = [
-        [ 1, 2, 3, 5, 4, 3, 2, 1],
-        [ 6, 6, 6, 6, 6, 6, 6, 6],
-        [ 0, 0, 0, 0, 0, 0, 0, 0],
-        [ 0, 0, 0, 0, 0, 0, 0, 0],
-        [ 0, 0, 0, 0, 0, 0, 0, 0],
-        [ 0, 0, 0, 0, 0, 0, 0, 0],
-        [12,12,12,12,12,12,12,12],
-        [ 7, 8, 9,11,10, 9, 8, 7]
+        [P.BlackRook, P.BlackKnight, P.BlackBishop, P.BlackQueen, P.BlackKing, P.BlackBishop, P.BlackKnight, P.BlackRook],
+        [P.BlackPawn, P.BlackPawn, P.BlackPawn, P.BlackPawn, P.BlackPawn, P.BlackPawn, P.BlackPawn, P.BlackPawn],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.WhitePawn, P.WhitePawn, P.WhitePawn, P.WhitePawn, P.WhitePawn, P.WhitePawn, P.WhitePawn, P.WhitePawn],
+        [P.WhiteRook, P.WhiteKnight, P.WhiteBishop, P.WhiteQueen, P.WhiteKing, P.WhiteBishop, P.WhiteKnight, P.WhiteRook]
       ];
       this.setState(state);
 
@@ -37,17 +38,17 @@
      */
     this.getState = function() {
       var state = [
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0]
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty],
+        [P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty, P.Empty]
       ];
       var $squares = $board.find('.square');
-      
+
       $squares.each(function(i, piece) {
         var row = parseInt(i / 8);
         var col = parseInt(i % 8);
@@ -84,7 +85,29 @@
       for(var i = newSquares.length-1; i >= 0; i--) {
         $board.append(newSquares[i]);
       }
+      self.setupPieceEvents();
     }
+
+    this.setupPieceEvents = function() {
+      $board.find('.square').click(self.selectPiece);
+    }
+
+    this.selectPiece = function() {
+      var square = $(this);
+      var piece = $(this).data('piece');
+      $board.find('.square').removeClass('highlight');
+      if (piece != P.Empty) {
+        var moves = ChessAI.Modules.States.getMovesForSquare(self.getState(), square.data('row'), square.data('col'));
+        for (var i = 0; i < moves.length; i++) {
+          $board.find(self.squareClass(moves[i][0], moves[i][1])).addClass('highlight');
+        }
+      }
+    };
+
+    this.squareClass = function(row, col) {
+      console.log('square_'+row+'x'+col);
+      return '.square_'+row+'x'+col;
+    };
 
     return this.init();
   };
